171fe99725de27dc7f1dc9953ab69cc3975a517c
Rich <richardpaul25s@gmail.com>
2017-12-08 15:12:23 -0500

    tea-house

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+alert("Hi, Welcome to Rich Schmitt Production Land. This is the Crystal Collector Game! Have fun playing...I Love You<3");
+
+$(".crystal-image").on("click", function()  {
+
+  alert("***You found a crystal!***");
+
+});
