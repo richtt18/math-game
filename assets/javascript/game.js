4ec37e83e1e986b416bdc42d3aef69fb58c95501
Rich <richardpaul25s@gmail.com>
2017-12-11 17:50:10 -0500

    12/11

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,89 @@
 alert("Hi, Welcome to Rich Schmitt Production Land. This is the Crystal Collector Game! Have fun playing...I Love You<3");
 
-$(".crystal-image").on("click", function()  {
+var targetNumber= 50;
 
-  alert("***You found a crystal!***");
+$("#numberToGuess").text(targetNumber);
+
+//var totalScore=;
+
+//var wins=;
+
+//var loses=;
+
+//var crystal_1= 17;
+//var crystal_2= 10;
+//var crystal_3= 5;
+//var crystal_4= 13;
+
+var counter = 0;
+$("#c1").on("click", function()  {
+  console.log(counter);
+
+  counter += 17;
+
+  alert("Your new crystal score count is:" + counter);
+
+  if(counter === targetNumber) {
+
+    alert("You won!");
+  }
+
+  else if(counter >= targetNumber) {
+    alert("You loose...");
+  }
+
+});
+
+$("#c2").on("click", function()  {
+  console.log(counter);
+
+  counter += 10;
+
+  alert("Your new crystal score count is:" + counter);
+
+  if(counter === targetNumber) {
+
+    alert("You won!");
+  }
+
+  else if(counter >= targetNumber) {
+    alert("You loose...");
+  }
+
+});
+
+$("#c3").on("click", function()  {
+  console.log(counter);
+
+  counter += 5;
+
+  alert("Your new crystal score count is:" + counter);
+
+  if(counter === targetNumber) {
+
+    alert("You won!");
+  }
+
+  else if(counter >= targetNumber) {
+    alert("You loose...");
+  }
+
+});
+
+$("#c4").on("click", function()  {
+  console.log(counter);
+
+  counter += 13;
+
+  alert("Your new crystal score count is:" + counter);
+
+  if(counter === targetNumber) {
+
+    alert("You won!");
+  }
+
+  else if(counter >= targetNumber) {
+    alert("You loose...");
+  }
 
 });
